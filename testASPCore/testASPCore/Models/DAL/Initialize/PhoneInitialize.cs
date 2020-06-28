fb8acf310a77414b3054f319589343601eaf68d5
soloviev-nikolay <soloviev_nikolay@mail.ru>
2020-06-28 20:51:17 +0500

    (#testASPCore) !

---
+++ testASPCore/testASPCore/Models/DAL/Initialize/PhoneInitialize.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using testASPCore.Models.DAL.Entity;
+
+namespace testASPCore.Models.DAL.Initialize
+{
+    public static class PhoneInitialize
+    {
+        public static void Initialize(DataBaseContext context)
+        {
+            if (!context.Phones.Any())
+            {
+                context.Phones.AddRange(
+                    new Phone
+                    {
+                        Name = "iPhone X",
+                        Company = "Apple",
+                        Price = 600
+                    },
+                    new Phone
+                    {
+                        Name = "Samsung Galaxy Edge",
+                        Company = "Samsung",
+                        Price = 550
+                    },
+                    new Phone
+                    {
+                        Name = "Pixel 3",
+                        Company = "Google",
+                        Price = 500
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ testASPCore/testASPCore/Services/PhoneService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using testASPCore.Models.DAL;
+using testASPCore.Models.DAL.Entity;
+
+namespace testASPCore.Services
+{
+    public class PhoneService
+    {
+        DataBaseContext db = new DataBaseContext();
+        
+        public IEnumerable<Phone> GetAll()
+        {
+            var result = db.Phones.AsEnumerable();
+            db.Dispose();
+            return result;
+        }
+
+        public Phone GetById(int? phoneId)
+        {
+            var result = db.Phones.Find(phoneId);
+            db.Dispose();
+            return result;
+        }
+
+        public void Create(Phone phone)
+        {
+            db.Phones.Add(phone);
+            db.SaveChanges();
+            db.Dispose();
+        }
+
+        public void Delete(int phoneId)
+        {
+            var result = db.Phones.Find(phoneId);
+            if (result != null)
+            {
+                db.Entry(result).State = EntityState.Deleted;
+                db.Phones.Remove(result);
+                db.SaveChanges();
+                db.Dispose();
+            }
+        }
+
+        public void Update(Phone phone)
+        {
+            db.Entry(phone).State = EntityState.Modified;
+            db.SaveChanges();
+            db.Dispose();
+        }
+    }
+}
