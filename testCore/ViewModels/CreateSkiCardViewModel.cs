7be805e36037c83918ab44d2f63a1a45ca0e9a25
soloviev-nikolay <soloviev_nikolay@mail.ru>
2020-07-03 18:14:19 +0500

    (#testASPCore) !

--- testCore/ViewModels/CreateSkiCardViewModel.cs
+++ testCore/ViewModels/CreateSkiCardViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,13 @@ namespace testCore.ViewModels
 {
     public class CreateSkiCardViewModel
     {
+        [DisplayName("Phone Number")]
         public string CardHolderPhoneNumber {get;set;}
+        [DisplayName("First Name")]
         public string CardHolderFirstName { get;set;}
+        [DisplayName("Last Name")]
         public string CardHolderLastName { get;set;}
+        [DisplayName("Birth Date")]
         public DateTime CardHolderBirthDate { get;set;}
     }
 }
